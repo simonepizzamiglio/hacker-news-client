418e49b8a002105cb1a365b46fabd6c8c77ce48c
Simone Pizzamiglio <me@simonepizzamiglio.com>
2024-07-29 21:10:13 +0200

    add e2e tests

--- e2e-tests/new-page.spec.ts
+++ e2e-tests/new-page.spec.ts
@@ -1,9 +1,39 @@
 import { test, expect } from "@playwright/test";
 
-const URL = "https://hacker-news-client-inky.vercel.app/";
+const URL = "https://hacker-news-client-inky.vercel.app";
 
 test("Redirects to /new", async ({ page }) => {
   await page.goto(URL);
   await expect(page).toHaveURL(`${URL}/new`);
   await page.getByRole("heading", { name: "New", exact: true }).isVisible();
+});
+
+test("Lists first 20 items", async ({ page }) => {
+  await page.goto(URL);
+  const articlesListLocator = await page.getByLabel("Articles list");
+  const countArticles = await articlesListLocator.getByRole("listitem").count();
+  await expect(countArticles).toBe(20);
+});
+
+test("Loads more items", async ({ page }) => {
+  await page.goto(URL);
+  await page.getByRole("link", { name: "More" }).click();
+  await page.waitForURL("**/new?page=2");
+  const articlesListLocator = await page.getByLabel("Articles list");
+
+  // Make sure skeletons are visible while loading elements
+  await expect(async () => {
+    const countArticleSkeletons = await articlesListLocator
+      .getByRole("status", { name: "Loading article" })
+      .count();
+    await expect(countArticleSkeletons).toBe(20);
+  }).toPass();
+
+  // Make sure another 20 items are fetched
+  await expect(async () => {
+    const countListItems = await articlesListLocator
+      .getByRole("listitem")
+      .count();
+    await expect(countListItems).toBe(40);
+  }).toPass();
 });
