02961016f110184682e853ff63baadf9630b21d8
Simone Pizzamiglio <me@simonepizzamiglio.com>
2024-07-30 10:48:38 +0200

    add e2e tests

--- e2e-tests/new-page.spec.ts
+++ e2e-tests/new-page.spec.ts
@@ -1,33 +1,94 @@
+import type { Page } from "@playwright/test";
 import { test, expect } from "@playwright/test";
 
 const URL = "https://hacker-news-client-inky.vercel.app";
 
-test("Redirects to /new", async ({ page }) => {
-  await page.goto(URL);
-  await expect(page).toHaveURL(`${URL}/new`);
-  await page.getByRole("heading", { name: "New", exact: true }).isVisible();
-});
-
-test("Lists first 20 items", async ({ page }) => {
-  await page.goto(URL);
+async function getInternalLink(page: Page, articlesCount: number) {
   const articlesListLocator = await page.getByLabel("Articles list");
-  const countArticles = await articlesListLocator.getByRole("listitem");
-  await expect(countArticles).toHaveCount(20);
-});
+  const link = await articlesListLocator
+    .locator('a:not([target="_blank"])')
+    .first();
 
-test("Loads more items", async ({ page }) => {
-  await page.goto(URL);
+  if (link) {
+    return link;
+  }
+
   await page.getByRole("link", { name: "More" }).click();
-  await page.waitForURL("**/new?page=2");
-  const articlesListLocator = await page.getByLabel("Articles list");
+  const countArticles = await articlesListLocator.getByRole("listitem");
+  const newArticlesCount = articlesCount + 20;
+  await expect(countArticles).toHaveCount(newArticlesCount);
 
-  // Make sure skeletons are visible while loading elements
-  const countArticleSkeletons = await articlesListLocator.getByRole("status", {
-    name: "Loading article",
+  getInternalLink(page, newArticlesCount);
+}
+
+test.describe("New page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(URL);
+    const loadingState = await page.getByRole("status", { name: "Loading..." });
+    await loadingState.isVisible();
+    await expect(loadingState).not.toBeVisible();
   });
-  await expect(countArticleSkeletons).toHaveCount(20);
 
-  // Make sure another 20 items are fetched
-  const countListItems = await articlesListLocator.getByRole("listitem");
-  await expect(countListItems).toHaveCount(40);
+  test("Redirects to /new", async ({ page }) => {
+    await expect(page).toHaveURL(`${URL}/new`);
+    await page.getByRole("heading", { name: "New", exact: true }).isVisible();
+  });
+
+  test("Lists first 20 items", async ({ page }) => {
+    const articlesListLocator = await page.getByLabel("Articles list");
+    const countArticles = await articlesListLocator.getByRole("listitem");
+    await expect(countArticles).toHaveCount(20);
+  });
+
+  test("Loads more items", async ({ page }) => {
+    await page.getByRole("link", { name: "More" }).click();
+    await page.waitForURL("**/new?page=2");
+    const articlesListLocator = await page.getByLabel("Articles list");
+
+    // Make sure skeletons are visible while loading elements
+    const countArticleSkeletons = await articlesListLocator.getByRole(
+      "status",
+      {
+        name: "Loading article",
+      },
+    );
+    await expect(countArticleSkeletons).toHaveCount(20);
+
+    // Make sure another 20 items are fetched
+    const countListItems = await articlesListLocator.getByRole("listitem");
+    await expect(countListItems).toHaveCount(40);
+  });
+
+  test("External link opens a new tab", async ({ page, context }) => {
+    const articlesListLocator = await page.getByLabel("Articles list");
+    const countArticles = await articlesListLocator.getByRole("listitem");
+    await expect(countArticles).toHaveCount(20);
+
+    const externalLink = await articlesListLocator
+      .locator('a[target="_blank"]')
+      .first();
+
+    if (externalLink) {
+      externalLink.click();
+
+      const [newPage] = await Promise.all([
+        context.waitForEvent("page"),
+        externalLink.click(), // Opens a new tab
+      ]);
+      await newPage.waitForLoadState();
+    }
+  });
+
+  test("Internal link opens article page", async ({ page }) => {
+    const articlesListLocator = await page.getByLabel("Articles list");
+    const countArticles = await articlesListLocator.getByRole("listitem");
+    await expect(countArticles).toHaveCount(20);
+
+    const internalLink = await getInternalLink(page, 20);
+
+    if (internalLink) {
+      internalLink.click();
+      await page.waitForURL("**/item/**");
+    }
+  });
 });
