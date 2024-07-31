cd0340bf72d9380d45a62ede2043baa118d73b94
Simone Pizzamiglio <me@simonepizzamiglio.com>
2024-07-31 16:02:36 +0200

    test: e2e tests for errors handling

---
+++ e2e-tests/errors.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+
+// const URL = "https://hacker-news-client-inky.vercel.app";
+const URL = "http://localhost:3000";
+
+test.describe("Errors", () => {
+  test("Handles page not found", async ({ page }) => {
+    await page.goto(`${URL}/this-page-does-not-exist`);
+    const title = await page.title();
+    expect(title).toBe("Not found");
+    await page.getByRole("heading", { name: "Not found" }).isVisible();
+  });
+
+  test("Handles item not found", async ({ page }) => {
+    await page.goto(`${URL}/item/789079273489472390432`);
+    const title = await page.title();
+    expect(title).toBe("Item not found");
+    await page.getByRole("heading", { name: "Not found" }).isVisible();
+  });
+});
